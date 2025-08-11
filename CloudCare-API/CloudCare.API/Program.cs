41d24b73ec1ae35d819ab21696a7d07664ddd994
hashirowais <hashir15@hotmail.com>
2025-08-11 01:20:05 -0600

    changed docker msg

--- CloudCare-API/CloudCare.API/Program.cs
+++ CloudCare-API/CloudCare.API/Program.cs
@@ -389,5 +389,5 @@ using (var scope = app.Services.CreateScope())
 app.Run();
 
 
-// docker build -t hashirowais/cloudcare-api:tag .  
-// docker run -d --name cloudcare-api -p 5001:5000 -e CONNECTION_STRING="Server=192.168.69.200;Port=5432;Database=Cloudcare_UAT;Username=CloudCare;Password=dw;" \ hashirowais/cloudcare-api:tag
+// docker build -t hashirowais/cloudcare-api:latest .  
+// docker run -d --name cloudcare-api -p 5001:5000 -e CONNECTION_STRING="Server=192.168.69.200;Port=5432;Database=Cloudcare_UAT;Username=CloudCare;Password=dw;" \ hashirowais/cloudcare-api:latest
